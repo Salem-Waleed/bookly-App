8ba877f2f0615cc220cfec6ae7591e99a9e69732
salem waleed <salembokir03@gmail.com>
2026-08-07 17:42:54 +0300

    adding sliding text to spalsh view

--- lib/features/splash/presentation/view/widget/spalsh_view_body.dart
+++ lib/features/splash/presentation/view/widget/spalsh_view_body.dart
@@ -1,23 +1,51 @@
 import 'package:bookly_app/core/Utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/view/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SpalshViewBody extends StatelessWidget {
+class SpalshViewBody extends StatefulWidget {
   const SpalshViewBody({super.key});
 
+  @override
+  State<SpalshViewBody> createState() => _SpalshViewBodyState();
+}
+
+class _SpalshViewBodyState extends State<SpalshViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+@override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children:  [
+      children: [
         Image.asset(AssetsData.logo),
-        const SizedBox(
-          height: 4,
-        ),
-        const Text('Read Free Books',
-        textAlign: TextAlign.center,
-        ),
+        const SizedBox(height: 4),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
