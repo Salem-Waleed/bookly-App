02bb5545c60fa7f1d1b77f53168a2cbd1e092587
salem waleed <salembokir03@gmail.com>
2026-08-11 23:23:27 +0300

    create book detail view

---
+++ lib/features/home/presentation/view/book_detaile_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/features/home/presentation/view/widget/book_detaile_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetaileView extends StatelessWidget {
+  const BookDetaileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const BookDetaileViewBody(),
+    );
+  }
+}
