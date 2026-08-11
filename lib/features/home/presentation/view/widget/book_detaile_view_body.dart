57c1bda4bef5b5955cbb969581578b5779d27541
salem waleed <salembokir03@gmail.com>
2026-08-12 00:36:34 +0300

    add custom book item to book view detaile

--- lib/features/home/presentation/view/widget/book_detaile_view_body.dart
+++ lib/features/home/presentation/view/widget/book_detaile_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/view/widget/custom_book_detaile_appbar.dart';
+import 'package:bookly_app/features/home/presentation/view/widget/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetaileViewBody extends StatelessWidget {
@@ -6,11 +7,18 @@ class BookDetaileViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width =  MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-        const CustomBookDetaileAppBar(),
+         CustomBookDetaileAppBar(),
+         Padding(
+           padding: EdgeInsets.symmetric(
+            horizontal: width * .17 ,
+            vertical: 15),
+           child: CustomBookItem(),
+         ),
         ],
       ),
     );
--- lib/features/home/presentation/view/widget/featured_list_view_items.dart
+++ lib/features/home/presentation/view/widget/featured_list_view_items.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/home/presentation/view/widget/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/view/widget/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -14,7 +14,7 @@ class FeaturedBooksListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: FeaturedlistViewItem(),
+            child: CustomBookItem(),
           );
         },
       ),
