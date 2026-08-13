b58e365f3bde294f6ceb08b7c538a4796933e14e
salem waleed <salembokir03@gmail.com>
2026-08-14 01:11:44 +0300

    create cubit

--- lib/features/home/data/repos/home_repo_imp.dart
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -30,8 +30,22 @@ class HomeRepoImp implements HomeRepo {
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks()async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?q=subject:computer&filter=free-ebooks',
+      );
+      List<BookModel> books = [];
+      for(var item in data['items']){
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } 
+    catch (e) {
+     if(e is DioException){
+      return left(ServerFailure.fromDioError(e));
+     }
+     return left(ServerFailure(e.toString()));
+    }
   }
 }
